b2aba9341f197f227bbe4193d398d25492b67b58
최원서 <dnjstj88@naver.com>
2024-01-26 13:29:41 +0900

    곱셈 실버1

--- 1주차/1629.cpp
+++ 1주차/1629.cpp
@@ -7,7 +7,6 @@ long long go(long long A, long long B)
     {
         return A % C;
     }
-
     long long a = go(A, B / 2);
     a = (a * a) % C;
     if (B % 2 == 1)
@@ -20,3 +19,6 @@ int main()
 
     cout << go(A, B) << "\n";
 }
+
+// 정수 범위 21억 생각하기
+// (A*B)%C == A%C * B%C; 생각하기.
