3816e878be9c30ce1a616894223cd16e2d80e672
최원서 <dnjstj88@naver.com>
2024-01-29 18:12:32 +0900

    그래프,DFS,BFS 구현

--- a.cpp
+++ a.cpp
@@ -1,25 +1,2 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int n = 3;
-int visited[n];
-vector<int> adj[n];
-
-
-void dfs(int u){
-    cout << u;
-    visited[u] = 1;
-    for(int i :adj[u]){
-        cout << i;
-        if (visited[i]) continue;
-        dfs(i);
-    }
-}
-
-int main(){
-    adj[1].push_back(2);
-    adj[1].push_back(3);
-    adj[1].push_back(4);
-    adj[2].push_back(5);
-    adj[2].push_back(6);
-    // dfs(1);
-}
