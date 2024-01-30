f40003e695736b06b9070f45b23db29366686f5e
최원서 <dnjstj88@naver.com>
2024-01-30 18:12:24 +0900

    사과담기게임 실버 5

--- a.cpp
+++ a.cpp
@@ -1,46 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, bits[65][65];
-// 재귀함수는 return의 위치 굉장히 중요함.
-void divide(int x, int y, int n)
+int N, M, J, K;
+int res = 0, start = 0;
+int main()
 {
-    if (n == 1)
+    cin >> N >> M;
+    cin >> J;
+    int end = M;
+    for (int i = 0; i < J; i++)
     {
-        cout << bits[x][y];
-        return;
-    }
-    int res = bits[x][y];
-
-    for (int i = x; i < x + n; i++)
-    {
-        for (int j = y; j < y + n; j++)
+        cin >> K;
+        if (start < K && K <= end)
         {
-            if (bits[i][j] != res)
+            continue;
+        }
+        else if (K <= start)
+        {
+            while (K != start + 1)
             {
-                cout << "(";
-                divide(x, y, n / 2);
-                divide(x, y + n / 2, n / 2);
-                divide(x + n / 2, y, n / 2);
-                divide(x + n / 2, y + n / 2, n / 2);
-                cout << ")";
-                return;
+                start -= 1;
+                end -= 1;
+                res += 1;
+            }
+        }
+        else if (K > end)
+        {
+            while (K != end)
+            {
+                start += 1;
+                end += 1;
+                res += 1;
             }
         }
     }
     cout << res;
-    return;
 }
-int main()
-{
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            scanf("%1d", &bits[i][j]);
-        }
-    }
-    divide(0, 0, N);
-}
-
-// 재귀함수. 같은 로직이 매개변수가 변하며 반복되는 경우 사용.
