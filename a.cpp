bea310ed5e2f656a1b06c79836d541ddf0b5a5e3
최원서 <dnjstj88@naver.com>
2024-01-30 14:19:09 +0900

    유기농 배추 실버2

--- a.cpp
+++ a.cpp
@@ -1,50 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M;
+// 풀기 전 connected component를 세는 문제이므로 DFS 활용하여 visited로 문제 풀기.
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-int visited[104][104];
-string line;
-int mp[104][104];
-int main()
+int mp[54][54];
+int visited[54][54];
+int M, N, K, y, x, tc;
+
+void DFS(int y, int x)
 {
-    cin >> N >> M;
-    for (int i = 0; i < N; i++)
+    visited[y][x] = 1;
+    for (int i = 0; i < 4; i++)
     {
-        cin >> line;
-        for (int j = 0; j < M; j++)
-        {
-
-            mp[i][j] = int(line[j]) - '0';
-        }
+        int nx = dx[i] + x;
+        int ny = dy[i] + y;
+        if (nx < 0 || ny < 0 || nx >= M || ny >= N || mp[ny][nx] == 0)
+            continue;
+        if (visited[ny][nx])
+            continue;
+        DFS(ny, nx);
     }
-
-    queue<pair<int, int>> q;
-    q.push({0, 0});
-    visited[0][0] = 1;
-    while (q.size())
-    {
-        int y, x;
-        tie(y, x) = q.front();
-        q.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || ny < 0 || nx >= M || ny >= N)
-                continue;
-            if (visited[ny][nx])
-                continue;
-            if (!mp[ny][nx])
-                continue;
-            q.push({ny, nx});
-            visited[ny][nx] = visited[y][x] + 1;
-        }
-    }
-    cout << visited[N - 1][M - 1];
 }
 
-// 맵이 그려져있는 구조이기 때문에 dx, dy를 선언하고 상하좌우를 비교하는 로직을 사용했다.
-// 지나야 하는 최소 칸 수( 최단거리 )를 구하는 문제이기 때문에 BFS를 활용하였다.
+int main()
+{
+    cin >> tc;
+    vector<int> result;
+    for (int j = 0; j < tc; j++)
+    {
+        cin >> M >> N >> K;
+        int num = 0;
+        for (int i = 0; i < N; i++)
+        {
+            for (int k = 0; k < M; k++)
+            {
+                mp[i][k] = 0;
+                visited[i][k] = 0;
+            }
+        }
 
-// tie를 이용하여 queue의 맨 앞 부분을 효율적으로 뽑아보았다.
+        for (int i = 0; i < K; i++)
+        {
+            cin >> x >> y;
+            mp[y][x] = 1;
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int k = 0; k < M; k++)
+            {
+                if (visited[i][k] == 0 && mp[i][k])
+                {
+                    DFS(i, k);
+                    num += 1;
+                }
+            }
+        }
+        result.push_back(num);
+    }
+    for (int i : result)
+    {
+        cout << i << "\n";
+    }
+}
+
+// 묹법사항
+// fill(배열 처음 주소, 매열 마지막 주소,0)를 이용해 0으로 채우기
